90bb6250fb711045426070f9acc002d8c95c9542
TBBJason <wujason19@gmail.com>
2026-09-22 20:24:18 -0400

    postgres, neon, sqlalchemy and psycog integration

---
+++ database/session.py
@@ -0,0 +1,18 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+database_url = os.environ["DATABASE_URL"]
+
+# explicitly set through psycopg3:
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
+
+
+engine = create_engine(
+    database_url,
+    pool_pre_ping=True,
+    pool_size=3,
+    max_overflow=2
+)
